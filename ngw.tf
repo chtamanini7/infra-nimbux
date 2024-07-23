9cf1cd0f599df47c63d662f3659934ae353818fa
chtamanini-jala <christian.tamanini@jala-foundation.org>
2024-07-23 12:07:05 -0300

    added snd natgateway

--- ngw.tf
+++ ngw.tf
@@ -2,10 +2,22 @@ resource "aws_eip" "nat_eip" {
   vpc = true
 }
 
+resource "aws_eip" "nat_eip_2" {
+  vpc = true
+}
+
 resource "aws_nat_gateway" "nat_gateway" {
   allocation_id = aws_eip.nat_eip.id
   subnet_id     = aws_subnet.public_subnet_1.id
   tags = {
     Name = "nat_gateway"
+  }
+}
+
+resource "aws_nat_gateway" "nat_gateway_2" {
+  allocation_id = aws_eip.nat_eip_2.id
+  subnet_id     = aws_subnet.public_subnet_2.id
+  tags = {
+    Name = "nat_gateway_2"
   }
 }
--- rt.tf
+++ rt.tf
@@ -14,6 +14,14 @@ resource "aws_route_table" "private_route_table" {
   }
 }
 
+resource "aws_route_table" "private_route_table_2" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway_2.id
+  }
+}
+
 resource "aws_route_table_association" "public_subnet_1_route" {
   subnet_id      = aws_subnet.public_subnet_1.id
   route_table_id = aws_route_table.public_route_table.id
